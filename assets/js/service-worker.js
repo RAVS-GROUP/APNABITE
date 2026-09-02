ca82d67bb11fe7c15e4fb02406ca5546e81acb0a
RAVS <shubham110267@gmail.com>
2026-09-02 23:34:50 +0530

    Update service-worker.js

--- assets/js/service-worker.js
+++ assets/js/service-worker.js
@@ -6,8 +6,8 @@
  */
 
 const CACHE_PREFIX = 'apnabite-';
-const STATIC_CACHE = CACHE_PREFIX + 'static-v1';
-const PAGE_CACHE = CACHE_PREFIX + 'pages-v1';
+const STATIC_CACHE = CACHE_PREFIX + 'static-v2';
+const PAGE_CACHE = CACHE_PREFIX + 'pages-v2';
 
 const APP_SHELL_FILES = [
   './',
